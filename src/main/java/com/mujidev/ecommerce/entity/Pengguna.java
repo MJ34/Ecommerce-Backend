4b4e51f1bfb73cdbaca315c4dc373096ee854473
MJ34 <kasur.rusak404@gmail.com>
2022-08-06 10:43:41 +0700

    first commit

---
+++ src/main/java/com/mujidev/ecommerce/entity/Pengguna.java
@@ -0,0 +1,38 @@
+package com.mujidev.ecommerce.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Pengguna implements Serializable {
+
+    @Id
+    private String id;
+    @JsonIgnore
+    private String password;
+    private String nama;
+    @JsonIgnore
+    private String alamat;
+    @JsonIgnore
+    private String email;
+    @JsonIgnore
+    private String noTelepon;
+    @JsonIgnore
+    private String roles;
+    @JsonIgnore
+    private Boolean isAktif;
+
+    public Pengguna(String username) {
+        this.id = username;
+    }
+    
+}
